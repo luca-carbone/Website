f54c9a1dc66a99ce972503b52b9edb354009deb8
luca-carbone <162471265+luca-carbone@users.noreply.github.com>
2024-03-07 16:48:45 +0100

    modifica

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,41 @@
-<script lang="ts">
-	import { get_collection } from '$cms';
+<script>
+	import CardMusica from '$components/CardMusica.svelte';
+
+	let dischi = [
+		{
+			artista: 'System of a Down',
+			disco: 'Toxicity',
+			durata: '44:25',
+			anno: '2001',
+			image: 'https://upload.wikimedia.org/wikipedia/en/6/64/SystemofaDownToxicityalbumcover.jpg'
+		},
+		{
+			artista: 'Tiberio',
+			disco: 'Sugar',
+			durata: '24:04',
+			anno: '2002',
+			image: 'https://upload.wikimedia.org/wikipedia/en/6/64/SystemofaDownToxicityalbumcover.jpg'
+		},
+		{
+			artista: 'Frank',
+			disco: 'prova',
+			durata: '33:04',
+			anno: '2023',
+			image: 'https://upload.wikimedia.org/wikipedia/en/6/64/SystemofaDownToxicityalbumcover.jpg'
+		}
+	];
 </script>
 
-Home page
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
+</svelte:head>
+
+{#each dischi as disco}
+	<CardMusica
+		artista={disco.artista}
+		disco={disco.disco}
+		durata={disco.durata}
+		anno={disco.anno}
+		image={disco.image}
+	/>
+{/each}
